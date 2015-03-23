2c0c733e59a34197daa4c1a1b573ba9845e561c5
Paul Pukach <pavlopukach@gmail.com>
2015-03-24 00:26:23 +0200

    ticket:1 fixed urls

--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from apps.personal_info.views import IndexView
+
 urlpatterns = patterns(
     '',
     # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', IndexView.as_view(), name='home'),
+    
     url(r'^admin/', include(admin.site.urls)),
 )
