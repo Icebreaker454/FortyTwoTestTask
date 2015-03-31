51e70e6dec4e18330ae647d5d6400b3ade26cd38
Paul Pukach <pavlopukach@gmail.com>
2015-03-31 23:15:44 +0300

    ticket:5 recovered mistakenly removed script

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -1,23 +1,23 @@
 function AsyncRequestUpdate() {
     var initial_title = $('title').text();
+    var count = parseInt($('#request-table').data('count'));
     var unread = 0;
     setInterval(function() {
         var table = $('#request-table');
-        var last_time = $('td:first').text();
         $.get(
             "/requests/",
             function(data){
                 table.html($(data).find('#request-table'));
                 table.attr("class", "table table-striped");
-                if($('td:first').text() != last_time) {
-                    unread++;
+                var new_count = parseInt($(data).find('#request-table').data('count'));
+                if(new_count > count) {
+                    unread = new_count - count;
                     document.title = '(' + unread + ')' + initial_title;
                 }
                 if(document.hasFocus()){
                     unread = 0;
-                }
-                if(unread == 0) {
                     document.title = initial_title;
+                    count = new_count;
                 }
             },
             'html'
