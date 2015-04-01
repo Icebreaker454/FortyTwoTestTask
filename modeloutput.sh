e0f71b15d76fb6332cb49cc100a2574ea14505b4
Paul Pukach <pavlopukach@gmail.com>
2015-04-01 23:18:45 +0300

    ticket:8 created bash script

---
+++ modeloutput.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+MANAGE=django-admin.py
+SETTINGS=fortytwo_test_task.settings
+
+DATE=`date +%Y-%m-%d.%H.%M.%S`
+FILE=$DATE.dat
+
+PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$SETTINGS $MANAGE modeloutput 1>$FILE
