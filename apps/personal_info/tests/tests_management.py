66cb9e07799b61b177f8f0199aa587e7618be4d2
Paul Pukach <pavlopukach@gmail.com>
2015-04-01 21:58:09 +0300

    ticket:8 added functional tests for management command

---
+++ apps/personal_info/tests/tests_management.py
@@ -0,0 +1,67 @@
+"""
+This module contains functional tests for the
+personal_info application management commands
+"""
+from StringIO import StringIO
+
+from django.core.management import call_command
+from django.test import TestCase
+from django.db import models
+
+
+class CommandsTest(TestCase):
+    """
+    This class is the personal_info management commands
+    test case
+    """
+    def setUp(self):
+        """
+        Initialize testing data
+        """
+        self.out_file = StringIO()
+        self.err_file = StringIO()
+
+        models_list = models.get_models()
+
+        self.model_data = [
+            '%s %s' % (
+                model.__name__,
+                model.objects.count()
+            ) for model in models_list
+        ]
+
+    def test_call_command(self):
+        """
+        Test the whole command functionality
+        """
+        call_command(
+            'modeloutput',
+            stdout=self.out_file,
+            stderr=self.err_file
+        )
+
+        self.out_file.seek(0)
+        self.err_file.seek(0)
+
+        self.assertEqual(
+            len(self.out_file.readlines()),
+            len(self.err_file.readlines())
+        )
+
+        for model_data_entry in self.model_data:
+            line = self.out_file.readline()
+            error_line = self.err_file.readline()
+            self.assertEqual('error: %s' % line, error_line)
+            data = line.strip().split('')
+            self.assertIn(data[0], model_data_entry)
+            self.assertIn(data[1], model_data_entry)
+
+        self.assertEqual(
+            len(self.out_file.readlines()),
+            len(self.model_data)
+        )
+
+
+
+
+
