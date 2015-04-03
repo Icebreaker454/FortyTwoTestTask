5612982a823e83b9f6eab1ff4d47a5540a5de24a
Paul Pukach <pavlopukach@gmail.com>
2015-04-03 11:09:27 +0300

    ticket:8 MORE MAKEFILE REORGANIZATIONS(2)!

--- modeloutput.sh
+++ modeloutput.sh
@@ -6,4 +6,4 @@ SETTINGS=fortytwo_test_task.settings
 DATE=`date +%Y-%m-%d.%H.%M.%S`
 FILE=$DATE.dat
 
-PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$SETTINGS $MANAGE modeloutput 2>$FILE
+PYTHONPATH=`pwd` DJANGO_SETTINGS_MODULE=$SETTINGS $MANAGE modeloutput 
