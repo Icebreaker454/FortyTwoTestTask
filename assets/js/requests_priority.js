0d6c381b44bc7b0a071aa8157c5d720a00370ea6
Paul Pukach <pavlopukach@gmail.com>
2015-04-04 15:18:56 +0300

    ticket:13 reworked AJAX posting security issues

--- assets/js/requests_priority.js
+++ assets/js/requests_priority.js
@@ -16,12 +16,14 @@ function initAjaxPriorityPosting() {
                 },
                 beforeSend: function () {
                     $('#loading-indicator').show()
+                    $('input').attr("disabled", "disabled");
                 },
                 success: function (data, status) {
                     $('#loading-indicator').hide();
+                    $('input').removeAttr("disabled");
                 },
                 error: function (status, error) {
-                    var row = $('#loading-indicator');
+                    var row = $('#loading-row');
                     row.css("background-color", "red");
                     row.html('<h4>Internal server error, please, try again later</h4>');
                 }
@@ -34,39 +36,44 @@ function initAjaxPagination() {
     var current_page = 1;
     var page_count = $('#request-table').data('pages');
     $('#load-more-button').mouseover(function() {
-        current_page++;
-        if(current_page <= page_count) {
-            $.ajax(
-                '/requests/edit_priority/',
-                {
-                    dataType: 'html',
-                    type: 'GET',
-                    data: {
-                        'page': current_page
-                    },
-                    beforeSend: function () {
-                        $('h4 #load-more-button').hide();
-                        $('#load-more').show();
-                    },
-                    success: function (data, status) {
-                        var table_body = $(data).find('#request-table > tbody');
-                        $('#request-table').append(table_body);
-                        $('#load-more').hide();
-                    },
-                    error: function (status, error) {
-                        var row = $('#loading-indicator');
-                        row.css("background-color", "red");
-                        row.html('<h4>Internal server error, please, try again later</h4>');
-                    }
-                }
-            );
-        }
-        else {
-            $('#load-more').show();
-            setTimeout(function() {
-                $('#load-more-button').hide();
-            }, 1000);
-        }
+        $('#load-more').show();
+        setTimeout( function() {
+            current_page++;
+            if (current_page <= page_count) {
+                setTimeout(function () {
+                    $.ajax(
+                        '/requests/edit_priority/',
+                        {
+                            dataType: 'html',
+                            type: 'GET',
+                            data: {
+                                'page': current_page
+                            },
+                            beforeSend: function () {
+                                $('h4 #load-more-button').hide();
+                                $('input').attr("disabled", "disabled");
+                            },
+                            success: function (data, status) {
+                                var table_body = $(data).find('#request-table > tbody');
+                                $('#request-table').append(table_body);
+                                $('#load-more').hide();
+                                $('input').removeAttr("disabled");
+                            },
+                            error: function (status, error) {
+                                var row = $('#loading-indicator');
+                                row.css("background-color", "red");
+                                row.html('<h4>Internal server error, please, try again later</h4>');
+                            }
+                        }
+                    );
+                }, 1000);
+            }
+            else {
+                setTimeout(function () {
+                    $('#load-more-button').hide();
+                }, 1000);
+            }
+        }, 100)
     });
 }
 
