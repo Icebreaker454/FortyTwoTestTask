7bd461d5bb8e5dc66f3a3a604b97363b1b184905
Paul Pukach <pavlopukach@gmail.com>
2015-04-04 20:58:05 +0300

    ticket:13 slightly improved AJAX load more button

--- assets/js/requests_priority.js
+++ assets/js/requests_priority.js
@@ -35,45 +35,42 @@ function initAjaxPriorityPosting() {
 function initAjaxPagination() {
     var current_page = 1;
     var page_count = $('#request-table').data('pages');
-    $('#load-more-button').mouseover(function() {
+    $('#load-more-button').click(function() {
         $('#load-more').show();
-        setTimeout( function() {
             current_page++;
             if (current_page <= page_count) {
-                setTimeout(function () {
-                    $.ajax(
-                        '/requests/edit_priority/',
-                        {
-                            dataType: 'html',
-                            type: 'GET',
-                            data: {
-                                'page': current_page
-                            },
-                            beforeSend: function () {
-                                $('h4 #load-more-button').hide();
-                                $('input').attr("disabled", "disabled");
-                            },
-                            success: function (data, status) {
-                                var table_body = $(data).find('#request-table > tbody');
-                                $('#request-table').append(table_body);
-                                $('#load-more').hide();
-                                $('input').removeAttr("disabled");
-                            },
-                            error: function (status, error) {
-                                var row = $('#loading-indicator');
-                                row.css("background-color", "red");
-                                row.html('<h4>Internal server error, please, try again later</h4>');
-                            }
+                $.ajax(
+                    '/requests/edit_priority/',
+                    {
+                        dataType: 'html',
+                        type: 'GET',
+                        data: {
+                            'page': current_page
+                        },
+                        beforeSend: function () {
+                            $('h4 #load-more-button').hide();
+                            $('input').attr("disabled", "disabled");
+                        },
+                        success: function (data, status) {
+                            var table_body = $(data).find('#request-table > tbody');
+                            $('#request-table').append(table_body);
+                            $('#load-more').hide();
+                            $('input').removeAttr("disabled");
+                        },
+                        error: function (status, error) {
+                            var row = $('#loading-indicator');
+                            row.css("background-color", "red");
+                            row.html('<h4>Internal server error, please, try again later</h4>');
                         }
-                    );
-                }, 1000);
+                    }
+                );
             }
             else {
                 setTimeout(function () {
                     $('#load-more-button').hide();
                 }, 1000);
             }
-        }, 100)
+
     });
 }
 
